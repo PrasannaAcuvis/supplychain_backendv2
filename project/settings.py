e5b675bacb6842ceac6dbcd86df3183d44972599
emmanuelprinceg <emmanuelprinceg@gmail.com>
2024-12-15 20:31:32 +0530

    allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hlw^)zb1cqr0($dh$tp6tki)9vzuwypxel_e599wneh%+zg9%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
